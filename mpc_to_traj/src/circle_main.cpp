a50bfd83fd11fd3e3b971a376094aa3bedc874c4
kimsooyoung <tge1375@naver.com>
2023-09-05 21:21:03 +0900

    circular main ongoing

--- mpc_to_traj/src/circle_main.cpp
+++ mpc_to_traj/src/circle_main.cpp
@@ -13,6 +13,8 @@ using std::endl;
 using std::map;
 using std::vector;
 using std::string;
+using std::sin;
+using std::cos;
 
 // For every steps
 // 1. get traj
@@ -26,25 +28,37 @@ int main() {
 
   const double pi = M_PI;
 
-  const int window_size = 10;
+  const int window_size = 9;
   const int iters = 200; // 200
   const double dt = 0.1;
 
   int traj_sampling_num = int(iters * dt);
-
-  // initial trajectory
 
   // Case 2. circular trajectory
   auto traj_points = GetTrajPointsCirc(0, window_size, iters);
   auto traj_x = std::get<0>(traj_points);
   auto traj_y = std::get<1>(traj_points);
 
+  // initial state
+  double x = 0.0;
+  double y = 0.0;
+  double psi = 0.0;
+  double v = 0.0;
+  double w = 0.0;
+  // We'll use robot frame, therefore cte = 0 is our goal.
+
   VectorXd x_veh(window_size);
   VectorXd y_veh(window_size);
+  double cospsi = cos(psi);
+  double sinpsi = sin(psi);
+
+  // initial trajectory
   for(long unsigned int i = 0; i < window_size; ++i)
   {
-    x_veh[i] = traj_x[i];
-    y_veh[i] = traj_y[i];
+    const double dx = traj_x[i] - x;
+    const double dy = traj_y[i] - y;
+    x_veh[i] = dx * cospsi + dy * sinpsi;
+    y_veh[i] = dy * cospsi - dx * sinpsi;
   }
 
   // 1rd order polynomial
@@ -52,17 +66,11 @@ int main() {
   // 3rd order polynomial
   // auto coeffs = polyfit(x_veh, y_veh, 3);
 
-  // initial state
-  double x = 0.0;
-  double y = 0.0;
-  // double y = 2.0;
-  double psi = 0.0;
-  double v = 0.0;
-  double w = 0.0;
-  // We'll use robot frame, therefore cte = 0 is our goal.
-  double cte = polyeval(coeffs, x) - y;
+  // double cte = polyeval(coeffs, x) - y;
+  double cte = polyeval(coeffs, 0.0);
   // linear approx for epsi
-  double epsi = psi - atan(coeffs[1]);
+  // double epsi = psi - atan(coeffs[1]);
+  double epsi = atan(coeffs[1]);
 
   // prepare parameters
   map<string, double> mpc_params;
@@ -75,7 +83,7 @@ int main() {
   mpc_params["MIN_ANG_ACC"] = -3.0;
   mpc_params["MAX_ANG_ACC"] = 3.0;
 
-  mpc_params["W_CTE"] = 500.0;
+  mpc_params["W_CTE"] = 100.0;
   mpc_params["W_EPSI"] = 100.0;
   mpc_params["W_V"] = 10.0;
   mpc_params["W_A"] = 1.0;
@@ -91,6 +99,9 @@ int main() {
   mpc.LoadParams(mpc_params);
 
   // output container
+  double x_world = 0;
+  double y_world = 0;
+  double psi_world = 0;
   vector<double> x_vals = {state[0]};
   vector<double> y_vals = {state[1]};
   vector<double> psi_vals = {state[2]};
@@ -104,21 +115,10 @@ int main() {
   // auto vars = mpc.Solve(state, coeffs);
   // return 0;
 
-  for (size_t i = 0; i < iters; ++i) {
-  // for (size_t i = 0; i < 50; ++i) {
+  // for (size_t i = 0; i < iters; ++i) {
+  for (size_t i = 0; i < 100; ++i) {
 
     auto vars = mpc.Solve(state, coeffs);
-
-    // cout << "x = " << vars[0] << endl;
-    // cout << "y = " << vars[1] << endl;
-    // cout << "psi = " << vars[2] << endl;
-    // cout << "v = " << vars[3] << endl;
-    // cout << "w = " << vars[4] << endl;
-    // cout << "cte = " << vars[5] << endl;
-    // cout << "epsi = " << vars[6] << endl;
-    // cout << "a = " << vars[7] << endl;
-    // cout << "alpha = " << vars[8] << endl;
-    // cout << endl;
 
     auto cur_x = vars[0];
     auto cur_y = vars[1];
@@ -130,9 +130,24 @@ int main() {
     auto cur_a = vars[7];
     auto cur_alpha = vars[8];
 
-    x_vals.push_back(cur_x);
-    y_vals.push_back(cur_y);
-    psi_vals.push_back(cur_psi);
+    x_world += cur_x * cos(psi_world);
+    y_world += cur_x * sin(psi_world);
+    psi_world += cur_psi;
+
+    cout << "x = " << x_world << endl;
+    cout << "y = " << y_world << endl;
+    cout << "psi = " << psi_world << endl;
+    cout << "v = " << vars[3] << endl;
+    cout << "w = " << vars[4] << endl;
+    cout << "cte = " << vars[5] << endl;
+    cout << "epsi = " << vars[6] << endl;
+    cout << "a = " << vars[7] << endl;
+    cout << "alpha = " << vars[8] << endl;
+    cout << endl;
+
+    x_vals.push_back(x_world);
+    y_vals.push_back(y_world);
+    psi_vals.push_back(psi_world);
     v_vals.push_back(cur_v);
     w_vals.push_back(cur_w);
     cte_vals.push_back(cur_cte);
@@ -140,29 +155,43 @@ int main() {
     a_vals.push_back(cur_a);
     alpha_vals.push_back(cur_alpha);
 
-    // update state
-    state << vars[0], vars[1], vars[2], vars[3], vars[4], vars[5], vars[6];
-  
+    // get new trajectory
     traj_points = GetTrajPointsCirc(i, window_size, iters);
     traj_x = std::get<0>(traj_points);
     traj_y = std::get<1>(traj_points);
 
-    // for (auto x : traj_x)
-    //     std::cout << "traj_x : " << x << std::endl;
-    // for (auto y : traj_y)
-    //     std::cout << "traj_y : " << y << std::endl;
+    // convert trajectories into robot frame
+    cospsi = cos(psi_world);
+    sinpsi = sin(psi_world);
 
-    // VectorXd x_veh(window_size);
-    // VectorXd y_veh(window_size);
-    for(long unsigned int i = 0; i < window_size; ++i)
+    for(int i = 0; i < window_size; i++) 
     {
-      x_veh[i] = traj_x[i];
-      y_veh[i] = traj_y[i];
+        x_veh[i] = traj_x[i] * cospsi + traj_y[i] * sinpsi - x_world;
+        y_veh[i] = -traj_x[i] * sinpsi + traj_y[i] * cospsi - y_world;
     }
+
+    // for(int i = 0; i < window_size; i++) 
+    // {
+    //     const double dx = traj_x[i] - cur_x;
+    //     const double dy = traj_y[i] - cur_y;
+    //     x_veh[i] = dx * cospsi + dy * sinpsi;
+    //     y_veh[i] = dy * cospsi - dx * sinpsi;
+    // }
 
     // 1rd order polynomial
     coeffs = polyfit(x_veh, y_veh, 1);
     // std::cout << "i : " << i << " / coeffs : " << coeffs << std::endl;
+
+    const double cte  = polyeval(coeffs, 0.0);
+    const double epsi = atan(coeffs[1]);
+
+    // update state
+    state << 0.0, 0.0, 0.0, cur_v, cur_w, cte, epsi;
+  
+    // for (auto x : traj_x)
+    //     std::cout << "traj_x : " << x << std::endl;
+    // for (auto y : traj_y)
+    //     std::cout << "traj_y : " << y << std::endl;
   }
 
   auto gt_traj = GetTrajPointsCirc(0, iters, iters);
